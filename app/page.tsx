fe3856870400e5d72630753a23eb629ecd832af0
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-11 23:11:38 +0530

    march initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,13 @@ export default function HomePage() {
 
 
   return (
-    <PostQuestion
-      question={question}
-      setQuestion={setQuestion}
-    />
+    <>
+      <h1 className="text-3xl font-bold text-center">Hello World</h1>
+      <PostQuestion
+        question={question}
+        setQuestion={setQuestion}
+      />
+    </>
   );
 }
 
