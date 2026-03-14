ddf96b557197748f7e0cfab9afed2931a301f1d3
Umair Ashraf <ashrafumair21@gmail.com>
2026-03-14 23:42:01 +0530

    placing a test commit

--- components/postQuestion.tsx
+++ components/postQuestion.tsx
@@ -6,7 +6,7 @@ import Suggestions from "@/components/suggestions";
 import { useState } from "react";
 
 export default function PostQuestion({ question, setQuestion }: any) {
-  const messyTemplate = `i dont know why this is not working i am trying to update count but its not changing properly sometimes it logs wrong value also i am using react and useState but maybe i am doing something wrong
+  const messyTemplate = `i dontt know why this is not working i am trying to update count but its not changing properly sometimes it logs wrong value also i am using react and useState but maybe i am doing something wrong
 
 import {useState} from "react"
 
